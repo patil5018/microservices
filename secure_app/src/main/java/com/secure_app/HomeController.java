3b09090eebd34d2a6ac8fdee0713b20dea67d428
Pratik <patil.pratik989098@gmail.com>
2021-11-19 12:37:17 +0530

    first commit

---
+++ secure_app/src/main/java/com/secure_app/HomeController.java
@@ -0,0 +1,13 @@
+package com.secure_app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "home.html";
+    }
+}
---
+++ secure_app/src/main/java/com/secure_app/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.secure_app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=repo.findByUsername(username);
+        if(user==null){
+            throw new UsernameNotFoundException("User not found try to sign up first");
+        }
+
+        return new UserPrincipal(user);
+    }
+}
